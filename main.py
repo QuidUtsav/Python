3dd5db1f97d956b011001dd26eac615372f10b93
QuidUtsav <122545941+QuidUtsav@users.noreply.github.com>
2025-03-19 21:18:06 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+
+def guess_num():
+    
+    secret_num= random.randint(1,100)
+    
+    print("Enter your guess from 1 to 100: ")
+    attempt=0
+    while True:
+        try:
+            guess=int(input("Your guess: "))
+            attempt+=1
+            if guess == secret_num:
+                print(f'You got it!!!🥳🥳in {attempt} tries')
+                break
+            elif guess>secret_num:
+                print("Higher than secret number")
+            else:
+                print("Lower than secret number")
+        
+        except ValueError:
+            print("Value error")
+            break
+            
+    choice=input("yes for playing again else enter any key: ").lower()
+    if(choice=='yes'):
+        guess_num()
+guess_num()
